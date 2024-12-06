b8625d76cf69ae2b8ead0eeebadc77c3e99a64c8
jesse-07-li <jessicalongane@Jessicas-MacBook-Air.local>
2024-12-06 11:07:58 +0100

    adding starting projects

---
+++ java.js
@@ -0,0 +1,13 @@
+var mylist = [
+	"apple","juice","grape","orange"];
+
+for (var k=0; k < mylist.length; k++) {
+	console.log (mylist[k] + "!");
+	// mylist.splice(2,2);
+	// mylist[k] = mylist[k] + "!";
+}
+
+var jump = 0; 
+while(jump < 10); {
+	console.log("you just did: " + jump)
+}
